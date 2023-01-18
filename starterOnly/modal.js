c3d52e983a803e23609b1ba02a0248d36e215c59
mathilde <mathilde.hetru@sarbacane.com>
2023-01-18 13:53:55 +0100

    start project

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,11 +1,19 @@
+//Click on icon NAV
+let editNavElement = document.querySelector(".icon");
+
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var topNav = document.getElementById("myTopnav");
+  if (topNav.className === "topnav") {
+    topNav.className += " responsive";
   } else {
-    x.className = "topnav";
+    topNav.className = "topnav";
   }
 }
+
+editNavElement.addEventListener("click", function(){
+  editNav();
+})
+
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
@@ -19,5 +27,4 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 
