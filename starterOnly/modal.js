e7bfd3c77e0e00a88ffc7227eef5bc92d7e89112
Mathilde Hetru <76809829+mathhetru@users.noreply.github.com>
2023-01-27 15:27:32 +0100

    location test

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,10 +17,9 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
 // Open form (with several button)
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-function launchModal() {
+modalBtn.forEach((btn) => btn.addEventListener("click", function() {
   modalBg.style.display = "block";
-};
+}));
 
 // Close form with cross
 const closeForm = document.querySelector(".close");
@@ -37,20 +36,23 @@ function closeModal() {
 };
 
 
+
+//------------------FORM-------------------------
 // Form validation
 const form = document.getElementById("form");
 
-// Empecher le submit automatique
+// prevent submit
 form.addEventListener("submit", function(e) {
   e.preventDefault();
 });
 
-// REGEX
-const nameRegExp = new RegExp("^[A-zÀ-ú \-]+$");
-const emailRegExp = new RegExp("^[a-zA-Z0-9_. -]+@[a-zA-Z.-]+[.]{1}[a-z]{2,10}$");
-const birthdateRegExp = new RegExp("^[0-9]{4}-[0-9]{2}-[0-9]{2}$");
+// regex
+const nameRegex = new RegExp("^[A-zÀ-ú \-]+$");
+const emailRegex = new RegExp("^[a-zA-Z0-9_. -]+@[a-zA-Z.-]+[.]{1}[a-z]{2,10}$");
+const birthdateRegex = new RegExp("^[0-9]{4}-[0-9]{2}-[0-9]{2}$");
+const quantityRegex = new RegExp("^[0-9]{1,2}$");
 
-// DOM des inputs du formulaire
+// DOM inputs from form
 const inputFirst = document.querySelector(".first");
 const inputLast = document.querySelector(".last");
 const inputEmail = document.querySelector(".email");
@@ -59,29 +61,48 @@ const inputQuantity = document.querySelector(".quantity");
 const inputLocation = document.querySelector(".location");
 const inputCondition = document.querySelector(".condition");
 
-// FUNCTION TEST DU REGEX SUR L'INPUT
-function testRegexOnInput(elem, regex, input) {
+// FUNCTION Test regex on input
+function testRegexOnInput(elem, regex, length, input) {
   elem.addEventListener('change', function(e) {
     const value = e.target.value;
     const valueLength = e.target.value.length;
-    if (regex.test(value) && valueLength >= 2){
+    if (regex.test(value) && valueLength >= length){
       input.dataset.errorVisible = "false";
     } else {
       input.dataset.errorVisible = "true";
     }
   });
-}
+};
 
-// Prénom, nom, email
-testRegexOnInput(form.first, nameRegExp, inputFirst); 
-testRegexOnInput(form.last, nameRegExp, inputLast);
-testRegexOnInput(form.email, emailRegExp, inputEmail);
+// FUNCTION Test input Location
+function testInputLocation() {
+    inputLocation.dataset.errorVisible = "false";
+    const severalLocation = document.getElementsByName("location");
+    for (var i = 0 ; i < severalLocation.length; i++) {
+      if (severalLocation[i].checked) {
+        inputLocation.dataset.errorVisible = "false";
+        break;
+      } else {
+        inputLocation.dataset.errorVisible = "true";
+      }
+    };
+};
 
-//date de naissance
-form.birthdate.addEventListener('change', function(e) {
-  const value = e.target.value;
-  console.log(e);
-  if (value == "yyyy-mm-dd") {
-    inputLast.dataset.errorVisible = "false";
+// FUNCTION Test checkbox of condition
+function testInputCondition() {
+  const inputConditionCheckbox = document.getElementById("checkbox1");
+  if (inputConditionCheckbox.checked) {
+    inputCondition.dataset.errorVisible = "false";
+  } else {
+    inputCondition.dataset.errorVisible = "true";
   }
-});
+};
+
+// firstname, name, email, birthdate, quantity, location and checkbox of condition
+testRegexOnInput(form.first, nameRegex, 2, inputFirst); 
+testRegexOnInput(form.last, nameRegex, 2, inputLast);
+testRegexOnInput(form.email, emailRegex, 2, inputEmail);
+testRegexOnInput(form.birthdate, birthdateRegex, 2, inputBirthdate);
+testRegexOnInput(form.quantity, quantityRegex, 1, inputQuantity);
+testInputLocation();
+testInputCondition();
