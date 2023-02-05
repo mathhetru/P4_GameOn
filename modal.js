c59ed6b6c193afe67d61852fc1b8598a7f1d5a84
Mathilde Hetru <76809829+mathhetru@users.noreply.github.com>
2023-02-05 11:50:56 +0100

    project ok

--- modal.js
+++ modal.js
@@ -1,12 +1,21 @@
 /** 
- * DOM ELEMENT
- * Icône du menu en responsive, <span> contenant ".icon"
+ * DOM ELEMENTS
+ * Icône du menu en responsive
+ * Fenêtre de la modal du formulaire et de remerciements.
+ * Boutons "Je m'inscris"
+ * Boutons croix de fermeture et bouton "fermer"
  */
 const editNavElement = document.querySelector(".icon");
+const modalBg = document.querySelector(".bground");
+const modalBgThx = document.querySelector(".bground-thanks");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const closeForm = document.querySelector(".close");
+const closeThanks = document.querySelectorAll(".closeThanks");
+
 
 /** 
- * FONCTION addEventListener "click"
- * ajoute ".responsive" sur la <div> qui contient "#mytopnav" lors du click sur la <span> ".icon"
+ * FONCTION
+ * en responsive, au click, ouvre la navigation
  */
 editNavElement.addEventListener("click", function () {
   const topNav = document.getElementById("myTopnav");
@@ -18,18 +27,8 @@ editNavElement.addEventListener("click", function () {
 });
 
 /** 
- * DOM ELEMENTS
- * Fenêtre de la modal du formulaire : <div> contenant .bground
- * Fenêtre de la modal de remerciement : <div> contenant .bground-thanks
- * Bouton "Je m'inscris": 2 <button> contenant .modal-btn (1 pour version desk et 1 pour version tablet/mobile)
- */
-const modalBg = document.querySelector(".bground");
-const modalBgThx = document.querySelector(".bground-thanks");
-const modalBtn = document.querySelectorAll(".modal-btn");
-
-/** 
- * FONCTION addEventListener "click"
- * ouvre le formulaire lors du click sur les <button>
+ * FONCTION
+ * ouvre le formulaire au click sur "je m'inscris"
  */
 modalBtn.forEach((btn) =>
   btn.addEventListener("click", function () {
@@ -38,21 +37,17 @@ modalBtn.forEach((btn) =>
 );
 
 /** 
- * DOM ELEMENT + FONCTION addEventListener "click"
- *  Icône croix : <span> contenant .close
- *  ferme la modal du formulaire avec la croix
+ * FONCTION
+ * ferme la modal du formulaire avec la croix
  */
-const closeForm = document.querySelector(".close");
 closeForm.addEventListener("click", function () {
   modalBg.style.display = "none";
 });
 
 /** 
- * DOM ELEMENT + FONCTION addEventListener
- *  Icône croix : <span> contenant .closeThanks
- *  ferme la modal de remerciement avec la croix ou avec le bouton "Fermer"
+ * FONCTION
+ * ferme la modal de remerciement avec la croix ou avec le bouton "Fermer"
  */
-const closeThanks = document.querySelectorAll(".closeThanks");
 closeThanks.forEach((btn) => btn.addEventListener("click", closeModal));
 function closeModal() {
   modalBgThx.style.display = "none";
@@ -60,10 +55,7 @@ function closeModal() {
 
 /** 
  * REGEX 
- * nameRegex pour le nom, prénom
- * emailRegex pour l'email
- * birthdateRegex pour la date de naissance
- * quantityRegex pour la quantité
+ * Les regex pour le nom, prénom, email, date de naissance et quantité
  */
 const nameRegex = new RegExp("^[A-zÀ-ú -]+$");
 const emailRegex = new RegExp("^[a-zA-Z0-9_. -]+@[a-zA-Z.-]+[.]{1}[a-z]{2,10}$");
@@ -72,14 +64,9 @@ const quantityRegex = new RegExp("^[0-9]{1,2}$");
 
 /** 
  * DOM ELEMENTS
- * input du prénom : <div> contenant .first
- * input du nom : <div> contenant .last
- * input de l'email : <div> contenant .email
- * input de la date de naissance : <div> contenant .birthdate
- * input de la quantité : <div> contenant .quantity
- * bouton radio pour le choix du tournoi : <div> contenant .location
- * les inputs contenant les différentes location : <input> contenant le name location
- * checkbox des conditions d'utilisation : <div> contenant .condition
+ * inputs du prénom, nom, email, date de naissance, quantité
+ * input bouton radio et les inputs contenant les différentes location
+ * checkbox des conditions d'utilisation
  */
 const inputFirst = document.querySelector(".first");
 const inputLast = document.querySelector(".last");
@@ -92,18 +79,17 @@ const inputCondition = document.querySelector(".condition");
 const inputConditionCheckbox = document.getElementById("checkbox1");
 
 /** 
- * FONCTION testRegexOnInput()
- * @param {string} input 
- * @param {string} regex 
- * @param {number} length 
- * @param {string} div 
- * @returns {boolean}
- * Teste le regex sur l'<input>
+ * FONCTION qui teste le regex sur l'input
  * reprend la value de l'input puis trim (rogne les espaces vides)
  * reprend la length de la value de l'input
  * SI le test regex sur la value est true ET si la longueur de la value est supérieure ou égale à length
  * ALORS on laisse le errorVisible à false (n'affiche pas l'erreur) PUIS return TRUE
  * SINON on passe le errorVisible à true (affiche l'erreur) PUIS return FALSE
+ * @param {string} input 
+ * @param {string} regex 
+ * @param {number} length 
+ * @param {string} div 
+ * @returns {boolean}
  */
 function testRegexOnInput(input, regex, length, div) {
   const value = input.value.trim();
@@ -118,7 +104,7 @@ function testRegexOnInput(input, regex, length, div) {
 }
 
 /**
- * FONCTION testInputLocation()
+ * FONCTION qui teste l'input location et qui permet de la valider
  * variable hasOneCheck de base à false
  * le errorVisible de l'input location de base à "false" (on n'affiche pas l'erreur)
  * on boucle for sur les locations
@@ -146,8 +132,8 @@ function testInputLocation() {
 }
 
 /**
- * permet de valider la checkbox condition
- * @returns {boolean} true is input ok then false
+ * FONCTION qui permet de valider la checkbox condition
+ * @returns {boolean} 
  */
 function testInputCondition() {
   if (inputConditionCheckbox.checked) {
@@ -160,7 +146,7 @@ function testInputCondition() {
 }
 
 /**
- * FONCTION addEventListener "change" sur chaque input du formulaire
+ * FONCTION sur chaque input du formulaire
  * avec la fonction testRegexOnInput qui contient les paramètres utiles
  */
 form.first.addEventListener("change", function (e) {
@@ -179,9 +165,9 @@ form.quantity.addEventListener("change", function (e) {
   testRegexOnInput(form.quantity, quantityRegex, 1, inputQuantity);
 });
 /**
- * on boucle for sur les locations
- * FONCTION addEventListener "change" sur chaque 
- * et FONCTION testInputLocation() sur chaque
+ * on boucle for sur les différentes locations
+ * FONCTION qui écoute le change  
+ * et FONCTION qui teste l'input location sur chacune
  */
 for (var i = 0; i < severalLocation.length; i++) {
   severalLocation[i].addEventListener("change", function () {
@@ -189,15 +175,15 @@ for (var i = 0; i < severalLocation.length; i++) {
   });
 }
 /**
- * FONCTION addEventListener "change"  sur l'input checkbox condition
- * et FONCTION testInputCondition()
+ * FONCTION qui écoute le change sur l'input checkbox condition
+ * et FONCTION qui teste l'input de la checkbox condition
  */
 inputConditionCheckbox.addEventListener("change", function () {
   testInputCondition();
 });
 
 /**
- * FONCTION addEventListener "submit" sur le form
+ * FONCTION qui écoute le submit sur le formulaitre
  * on empêche le comportement par défaut
  * SI toutes les FONCTIONS testRegexOnInput() + testInputLocation() + testInputCondition() return TRUE 
  * ALORS on ferme la modal du formulaire et on ouvre la modal de remerciement
