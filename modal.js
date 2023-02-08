80ca27ac2d980342f3862922aeaa7f5d841a4ef8
Mathilde Hetru <76809829+mathhetru@users.noreply.github.com>
2023-02-08 18:38:51 +0100

    corrections commentaires

--- modal.js
+++ modal.js
@@ -1,7 +1,7 @@
 /** 
- ** DOM ELEMENTS
+ * DOM ELEMENTS
  * Icône du menu en responsive
- * Fenêtre de la modal du formulaire et de remerciements.
+ * Fenêtre de la modal du formulaire et de remerciements
  * Boutons "Je m'inscris"
  * Boutons croix de fermeture et bouton "fermer"
  */
@@ -14,7 +14,6 @@ const closeThanks = document.querySelectorAll(".closeThanks");
 
 
 /** 
- ** FONCTION
  * en responsive, au click, ouvre la navigation
  */
 editNavElement.addEventListener("click", function () {
@@ -27,7 +26,6 @@ editNavElement.addEventListener("click", function () {
 });
 
 /** 
- ** FONCTION
  * ouvre le formulaire au click sur "je m'inscris"
  */
 modalBtn.forEach((btn) =>
@@ -37,7 +35,6 @@ modalBtn.forEach((btn) =>
 );
 
 /** 
- ** FONCTION
  * ferme la modal du formulaire avec la croix
  */
 closeForm.addEventListener("click", function () {
@@ -45,7 +42,6 @@ closeForm.addEventListener("click", function () {
 });
 
 /** 
- ** FONCTION
  * ferme la modal de remerciement avec la croix ou avec le bouton "Fermer"
  */
 closeThanks.forEach((btn) => btn.addEventListener("click", closeModal));
@@ -54,7 +50,7 @@ function closeModal() {
 }
 
 /** 
- ** REGEX 
+ * REGEX 
  * Les regex pour le nom, prénom, email, date de naissance et quantité
  */
 const nameRegex = new RegExp("^[A-zÀ-ú -]+$");
@@ -63,7 +59,7 @@ const birthdateRegex = new RegExp("^[0-9]{4}-[0-9]{2}-[0-9]{2}$");
 const quantityRegex = new RegExp("^[0-9]{1,2}$");
 
 /** 
- ** DOM ELEMENTS
+ * DOM ELEMENTS
  * inputs du prénom, nom, email, date de naissance, quantité
  * input bouton radio et les inputs contenant les différentes location
  * checkbox des conditions d'utilisation
@@ -79,12 +75,8 @@ const inputCondition = document.querySelector(".condition");
 const inputConditionCheckbox = document.getElementById("checkbox1");
 
 /** 
- ** FONCTION qui teste le regex sur l'input
- * reprend la value de l'input puis trim (rogne les espaces vides)
- * reprend la length de la value de l'input
- * SI le test regex sur la value est true ET si la longueur de la value est supérieure ou égale à length
- * ALORS on laisse le errorVisible à false (n'affiche pas l'erreur) PUIS return TRUE
- * SINON on passe le errorVisible à true (affiche l'erreur) PUIS return FALSE
+ * FONCTION qui teste le regex sur l'input
+ * et permet de le valider avec un nombre de caractères minimum
  * @param {string} input 
  * @param {string} regex 
  * @param {number} length 
@@ -104,15 +96,7 @@ function testRegexOnInput(input, regex, length, div) {
 }
 
 /**
- ** FONCTION qui teste l'input location et qui permet de la valider
- * variable hasOneCheck de base à false
- * le errorVisible de l'input location de base à "false" (on n'affiche pas l'erreur)
- * on boucle for sur les locations
- * SI un des location est check 
- * ALORS on passe hasOneCheck à true
- * on sort de la boucle et return true
- * SI hasOneCheck n'est pas passé à true 
- * ALORS on passe le errorVisible à true (on affiche l'erreur) PUIS on return false
+ * FONCTION qui teste l'input location et qui permet de la valider
  * @returns {boolean}
  * 
  */
@@ -132,7 +116,7 @@ function testInputLocation() {
 }
 
 /**
- ** FONCTION qui permet de valider la checkbox condition
+ * FONCTION qui permet de valider la checkbox condition
  * @returns {boolean} 
  */
 function testInputCondition() {
@@ -146,7 +130,7 @@ function testInputCondition() {
 }
 
 /**
- ** FONCTION sur chaque input du formulaire
+ * Ecoute du changement sur chaque input du formulaire
  * avec la fonction testRegexOnInput qui contient les paramètres utiles
  */
 form.first.addEventListener("change", function (e) {
@@ -165,10 +149,9 @@ form.quantity.addEventListener("change", function (e) {
   testRegexOnInput(form.quantity, quantityRegex, 1, inputQuantity);
 });
 /**
- * on boucle for sur les différentes locations
- ** FONCTION qui écoute le change  
- * et
- ** FONCTION qui teste l'input location sur chacune
+ * On boucle for sur les différentes locations
+ * et on écoute le change  
+ * et on teste l'input location sur chacune
  */
 for (var i = 0; i < severalLocation.length; i++) {
   severalLocation[i].addEventListener("change", function () {
@@ -176,7 +159,7 @@ for (var i = 0; i < severalLocation.length; i++) {
   });
 }
 /**
- ** FONCTION qui écoute le change sur l'input checkbox condition
+ * On écoute le change sur l'input checkbox condition
  * et FONCTION qui teste l'input de la checkbox condition
  */
 inputConditionCheckbox.addEventListener("change", function () {
@@ -184,11 +167,9 @@ inputConditionCheckbox.addEventListener("change", function () {
 });
 
 /**
- ** FONCTION qui écoute le submit sur le formulaitre
- * on empêche le comportement par défaut
- * SI toutes les FONCTIONS testRegexOnInput() + testInputLocation() + testInputCondition() return TRUE 
- * ALORS on ferme la modal du formulaire et on ouvre la modal de remerciement
- * ET on reset les inputs
+ * On écoute le submit sur le formulaire
+ * Si formulaire valide, affiche la modal de remerciements
+ * Sinon affiche les erreurs dans le formulaire
  */
 form.addEventListener("submit", function (e) {
   e.preventDefault();
